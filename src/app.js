3e0d6a628a1937e789828a19b4149c53e3133fa4
D-4costa <danielleelisabeth01@gmail.com>
2026-04-03 18:48:39 -0500

    fix 3

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   return res.redirect("/api-docs");
 });
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument));
 app.use(apiRoutes);
 
 app.use((req, res) => {
